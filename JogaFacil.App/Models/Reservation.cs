e56648dce9ef69ecedf70025af3b4fabf811e0b5
Pedro Valentim <pedrohvs2002@gmail.com>
2020-06-03 22:48:57 -0300

    Added reservation model

---
+++ JogaFacil.App/Models/Reservation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JogaFacil.App.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+
+        public Place Place { get; set; }
+
+        public Arena Arena { get; set; }
+
+        public User User { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public ReservationStatus Status { get; set; }
+    }
+
+    public enum ReservationStatus
+    {
+        WaitingApprovement = 0,
+        Approved = 1,
+        Denied = 2,
+        Canceled = 3
+    }
+}
