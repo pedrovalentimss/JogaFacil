b84825ae6c406465373c8d7a5e4d7506a8fe9b8f
Pedro Valentim <pedrohvs2002@gmail.com>
2020-06-27 19:48:05 -0300

    Added app settings class

---
+++ JogaFacil.Api/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JogaFacil.Api
+{
+    public class AppSettings
+    {
+        public string GeocodingApiUrl { get; set; }
+        public string GeocodingApiToken { get; set; }
+    }
+}
--- JogaFacil.Api/Startup.cs
+++ JogaFacil.Api/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using JogaFacil.Api.Data;
+using JogaFacil.Api.Services;
 
 namespace JogaFacil.Api
 {
@@ -38,6 +39,12 @@ namespace JogaFacil.Api
                                                             .AllowAnyMethod()
                                                             .AllowAnyHeader());
             });
+
+            services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+
+            services.AddHttpClient<IGeocodingService, GeocodingService>();
+
+            //services.AddSingleton<IGeocodingService, GeocodingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
