97ff408eb0ba01e96d823acf651a0d74f3c3a862
Pedro Valentim <pedrohvs2002@gmail.com>
2020-06-27 19:48:41 -0300

    Removed old geocoding client and test project

--- JogaFacil.Tests/GeocodingTests.cs
+++ JogaFacil.Tests/GeocodingTests.cs
@@ -2,6 +2,9 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using JogaFacil.Api.Clients;
 using System.Net.Http;
 using JogaFacil.Api.Entities;
+using Microsoft.Extensions.Options;
+using JogaFacil.Api;
+using Moq;
 
 namespace JogaFacil.Tests
 {
@@ -11,9 +14,6 @@ namespace JogaFacil.Tests
         [TestMethod]
         public void GetCoordinates()
         {
-            HttpClient httpClient = new HttpClient();
-            GeocodingClient geocodingClient = new GeocodingClient(httpClient);
-
             var address = new Address
             {
                 Street = "1600 Pennsylvania Ave NW",
@@ -24,10 +24,6 @@ namespace JogaFacil.Tests
                 State = "DC", 
                 Country = ""
             };
-
-            var coordinates = geocodingClient.GetCoordinatesFromAddress(address);
-            var lat = coordinates.Result.Latitude;
-            var lng = coordinates.Result.Longitude;
         }
     }
 }
