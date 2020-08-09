de951e6d577fbe4e0b16a80a9f014290728d7d6e
Pedro Valentim <pedrohvs2002@gmail.com>
2020-08-09 01:04:24 -0300

    Created alert system

---
+++ JogaFacil.App/wwwroot/scripts/alert.js
@@ -0,0 +1,15 @@
+﻿window.showAlert = (elementId, time) => {
+    console.log(elementId)
+    if (document.querySelector(elementId)) {
+        $(document).ready(function () {
+            $(elementId).show('fast');
+
+            console.log(time);
+            if (time) {
+                setTimeout(function () {
+                    $(elementId).hide('fast');
+                }, time);
+            }
+        });
+    }
+}
